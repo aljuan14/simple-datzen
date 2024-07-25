1c56be0b490c3e38c014f9ceb287f950c9fb6970
R_loads <90552316+aljuan14@users.noreply.github.com>
2024-07-25 23:56:58 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const PORT = 3000;
-const TITLE = process.env.TITLE || 'Default Title by farhanaaa qonita';
+const TITLE = process.env.TITLE || 'Default Title by danzi love kayla';
 const MESSAGE = process.env.MESSAGE || 'Default Message farhan love zakia';
 
 
